30c4ea432193ac3c5c0cc746d7b53cd11ff19449
Anthony <Kain235@gmail.com>
2022-05-17 19:32:10 -0400

    commit tree e5a3afb10cbb6100ccadc26934ef0022e03c5958

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [itemState, setItemState] = useState("")
+  let buttonText = () => {
+    if (itemState === "in-cart") {
+      return "Remove From Cart"
+    } else {
+      return "Add to Cart"
+    }
+  }
+
   return (
-    <li className="">
+    <li className={itemState}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={() => stateToggle("in-cart")}>{buttonText()}</button>
     </li>
   );
-}
 
+  function stateToggle() {
+    if (itemState === "") {
+      setItemState("in-cart")
+    } else {
+      setItemState("")
+    }
+  }
+
+
+}
 export default Item;
